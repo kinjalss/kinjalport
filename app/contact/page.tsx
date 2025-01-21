b8c576c4da1eb502008a2e04e4350fbcd2c74fb3
Kinjal Surve <kinjalsurvepune@gmail.com>
2025-01-21 11:58:56 +0530

    email correction

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,7 +7,7 @@ export default function Contact() {'use client'
         I'm always open to new opportunities and collaborations. Feel free to reach out to me using the email  below 
         or through my social media profiles.
       </p>
-     <a href="mailto:kinjalsuvepune@gmail.com"><label htmlFor="name" className="block text-gray-700 font-bold mb-2"><u>kinjalsuvepune@gmail.com</u></label></a>
+     <a href="mailto:kinjalsurvepune@gmail.com"><label htmlFor="name" className="block text-gray-700 font-bold mb-2"><u>kinjalsuvepune@gmail.com</u></label></a>
      < a href="https://www.linkedin.com/in/kinjal-surve-b638b1288/"><label htmlFor="name" className="block text-gray-700 font-bold mb-2"><u>LinkedIn</u></label></a>
      
       </div>
