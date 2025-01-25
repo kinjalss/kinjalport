92fa10bbbb9c26a750758711d84d83c141e27703
Kinjal Surve <kinjalsurvepune@gmail.com>
2025-01-25 16:41:10 +0530

    linkedin badge

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,8 +9,7 @@ export default function Contact() {'use client'
       </p>
      <a href="mailto:kinjalsurvepune@gmail.com"><label htmlFor="name" className="block text-gray-700 font-bold mb-2"><u>kinjalsurvepune@gmail.com</u></label></a>
      
-     //< a href="https://www.linkedin.com/in/kinjal-surve-b638b1288/"><label htmlFor="name" className="block text-gray-700 font-bold mb-2"><u>LinkedIn</u></label></a>
-     <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="medium" data-theme="light" data-type="VERTICAL" data-vanity="kinjal-surve-b638b1288" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://in.linkedin.com/in/kinjal-surve-b638b1288?trk=profile-badge">Kinjal Surve</a></div>
+     <a href="https://www.linkedin.com/in/kinjal-surve-b638b1288/"><label htmlFor="name" className="block text-gray-700 font-bold mb-2"><u>LinkedIn</u></label></a>
               
 
       </div>
